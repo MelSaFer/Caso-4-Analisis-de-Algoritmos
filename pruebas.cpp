7e61cb92056b330cd2deb70264792118eb1aade6
MelSaFer <dhnmelfer@estudiantec.cr>
2022-05-21 14:28:58 -0600

    Modificaciones en clase cuadrante, nuevas pruebas

--- pruebas.cpp
+++ pruebas.cpp
@@ -19,17 +19,27 @@ using namespace std;
 
 
 int main (){ 
+
+    
     Quadrant* q = new Quadrant();
 
     //Values we are going to use for reading the image
     int width, height, channels;
     
     //Load the image
-    unsigned char *image = stbi_load("images/prueba.jpg", &width, &height, &channels,0);
+    unsigned char *image = stbi_load("images/rostro1.jpg", &width, &height, &channels,0);
     size_t imageSize = width*height*channels;
 
     //Pixel* p = new Pixel();
-    q->addPixel(10, 21);
+    const size_t RGB = 3;
+    int xPosition = 3, yPosition = 80;
+    size_t index = RGB * (yPosition * width + xPosition);
+    cout <<  " 1" << endl;
+    int red = image[index + 0];  //red value
+    int green = image[index + 1]; //green value
+    int blue = image[index + 2]; //blue value
+    //cout << red << " " << green << " " << blue << endl;
+    q->addPixel(xPosition, yPosition, red, green, blue);
 
     //Clean memory
     delete q;
